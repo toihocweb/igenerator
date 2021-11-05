19389c149560124d00963687615af701e54b6228
Nhat Bui <buidinh.nhat@petronas.com>
2021-11-05 11:29:19 +0700

    #fix responsive

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -12,29 +12,38 @@ const { Sider } = Layout;
 const { Title } = Typography;
 
 function Sidebar() {
+  console.log(window.location.pathname);
   const [collapsed, setCollapsed] = useState(false);
-
   const onCollapse = (collapsed) => {
     setCollapsed(collapsed);
   };
 
   return (
-    <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+    <Sider
+      breakpoint={"sm"}
+      collapsible
+      collapsed={collapsed}
+      onCollapse={onCollapse}
+    >
       <div className="logo">
         <Title level={3} style={{ color: "white", marginBottom: 0 }}>
           {collapsed ? "IG" : "IGenerator"}
         </Title>
       </div>
-      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-        <Menu.Item key="1" icon={<KeyOutlined />}>
+      <Menu
+        theme="dark"
+        defaultSelectedKeys={[window.location.pathname]}
+        mode="inline"
+      >
+        <Menu.Item key="/" icon={<KeyOutlined />}>
           <NavLink to="/">Password Generator</NavLink>
         </Menu.Item>
-        <Menu.Item key="2" icon={<NumberOutlined />}>
+        <Menu.Item key="/random-bytes" icon={<NumberOutlined />}>
           <NavLink to="/random-bytes">Random Bytes</NavLink>
         </Menu.Item>
-        <Menu.Item key="3" icon={<DeploymentUnitOutlined />}>
-          <NavLink to="/docker-generator">Docker Generator (beta)</NavLink>
-        </Menu.Item>
+        {/*<Menu.Item key="/docker-generator" icon={<DeploymentUnitOutlined />}>*/}
+        {/*  <NavLink to="/docker-generator">Docker Generator (beta)</NavLink>*/}
+        {/*</Menu.Item>*/}
       </Menu>
     </Sider>
   );
